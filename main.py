72d962570011692f4dd19a60610042fce4cffd23
ike tohru <rokujyouhitomajp@gmail.com>
2014-01-07 00:16:26 +0900

    update

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ class REPL(object):
 
     def mainloop(self):
         while True:
-            self.printer.print_out(self.evaluator.eval(self.reader.read()))
+            self.printer.printOut(self.evaluator.eval(self.reader.read()))
 
 
 class Reader(object):
 
     def __init__(self):
         self.bye = 'bye'
+        self.prompt = 'repl> '
 
-    def read(self, prompt='repl > '):
+    def read(self, prompt=None):
+        prompt = prompt if prompt else self.prompt
         try:
             s = raw_input(prompt)
         except KeyboardInterrupt:
@@ -38,7 +40,7 @@ class Evaluator(object):
 
     def eval(self, s):
         tokens = self.parse(self.tokenize(s))
-        return 'eval'
+        return tokens
 
     def tokenize(self, s):
         return self.tokenizer.tokenize(s)
@@ -49,22 +51,39 @@ class Evaluator(object):
 
 class Tokenizer(object):
 
-    def tokenize(self, codes):
-        tokens = codes
-        #token = (typeid, type, value, line)
+    YES = 1 #'y'
+    NO = 2  #'n'
+    NUMBER = 3
+
+    def tokenize(self, s):
+        src = s.strip().split()
+        tokens = []
+        while len(src):
+            t = src.pop(0)
+            if t.isdigit():
+                tokens.append((Tokenizer.NUMBER, int(t)))
+            elif t == 'y':
+                tokens.append((Tokenizer.YES, t))
+            elif t == 'n':
+                tokens.append((Tokenizer.NO, t))
+            else:
+                pass
         return tokens
 
 
 class Parser(object):
 
     def parse(self, tokens):
+        if len(tokens) == 0:
+            pass
         return tokens
 
 
 class Printer(object):
 
-    def print_out(self, s):
+    def printOut(self, s):
         sys.stdout.write('%s\n' % s)
+        pass
 
 if __name__ == '__main__':
     repl = REPL(Reader(), Evaluator(Tokenizer(), Parser()), Printer())
